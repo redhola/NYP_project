c2c4f2bc755bb8d3d01939377805f8f3b40232ac
MehmetEfeGosterici <m.efegost558@gmail.com>
2021-03-27 17:20:28 +0300

    scripts

---
+++ move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    public float movementSpeed = 7;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var movementX = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movementX,0,0) * Time.deltaTime * movementSpeed;
+        var movementY = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0,movementY,0) * Time.deltaTime *movementSpeed;
+
+        animator.SetFloat("SpeedX", Mathf.Abs(movementX));
+        animator.SetFloat("SpeedY", Mathf.Abs(movementY));
+
+        Vector3 characterFlip = transform.localScale;
+        if( Input.GetAxis("Horizontal") < 0)
+            characterFlip.x = -1;
+        if( Input.GetAxis("Horizontal") > 0)
+            characterFlip.x = 1;
+        transform.localScale = characterFlip;
+    }
+}
---
+++ node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class node : MonoBehaviour
+{
+    private GameObject turret;
+    public Sprite TurretSprite;
+    
+    void OnMouseDown(){
+        if(turret != null){
+            Debug.Log("already a turret build");
+            return;
+        }
+        GetComponent<SpriteRenderer>().sprite = TurretSprite;
+
+    }
+    void OnMouseEnter(){
+        GetComponent<SpriteRenderer>().material.color = Color.blue;
+        Debug.Log("mouse over");
+    }
+    void OnMouseExit(){
+        GetComponent<SpriteRenderer>().material.color = Color.white;
+        Debug.Log("mouse exit");
+    }
+}
