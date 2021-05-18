41481b60c6e6673f56198f5c13a0ed76d2b4d57a
yunuskibar777 <yunuskibar777@gmail.com>
2021-05-18 14:03:22 +0300

    menus
    different variations of menus

---
+++ levelSelect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class levelSelect : MonoBehaviour
+{
+
+    public SceneFader fader;
+
+    public void Select (string levelName)
+    {
+
+        fader.FadeTo(levelName);
+
+    }
+  
+
+
+    public void level01()
+    {
+
+        SceneManager.LoadScene("Level01");
+
+    }
+    public void level02()
+    {
+
+        SceneManager.LoadScene("Level02");
+
+    }
+    
+
+
+}
---
+++ pauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+     public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        
+
+    }
+    public void LoadMenu()
+    {
+
+        SceneManager.LoadScene("Main menu");
+    }
+    public void QuitGame()
+    {
+
+        Application.Quit();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+        
+    }
+}
